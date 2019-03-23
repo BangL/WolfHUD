bf82e2b2ce02f3daed943e74221fc39e8894ff10
Henno Rickowski <henno.rickowski@gmail.com>
2019-03-23 13:14:17 +0100

    fix: GetOption -> getSetting

--- lua/NoBotCatch.lua
+++ lua/NoBotCatch.lua
@@ -2,7 +2,7 @@
 local _update_throw_link_original = CarryData._update_throw_link
 
 function CarryData:_update_throw_link(...)
-	if not JimHUD:GetOption({"EQUIPMENT", "DISABLE_BOT_CATCH"}, true) then
+	if not JimHUD:getSetting({"EQUIPMENT", "DISABLE_BOT_CATCH"}, true) then
 		return _update_throw_link_original(self, ...)
 	end
 end
