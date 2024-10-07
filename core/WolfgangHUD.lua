327ae08a2c13e1824b06ba6dcda72cb56efc1107
BangL <henno.rickowski@gmail.com>
2024-10-08 00:04:46 +0200

    get super

--- core/WolfgangHUD.lua
+++ core/WolfgangHUD.lua
@@ -123,13 +123,8 @@ if not _G.WolfgangHUD then
 	end
 
 	function WolfgangHUD:getVersion()
-		if BeardLib then
-			local mod = BeardLib.Frameworks.Base:GetModByName(WolfgangHUD.identifier or "")
-			return tostring(mod and mod.AssetUpdates and mod.AssetUpdates.version or "(n/a)")
-		elseif BLT then
-			local mod = BLT.Mods:GetMod(WolfgangHUD.identifier or "")
-			return tostring(mod and mod:GetVersion() or "(n/a)")
-		end
+		local mod = BLT.Mods:GetMod(WolfgangHUD.identifier or "")
+		return tostring(mod and mod:GetVersion() or "(n/a)")
 	end
 
 	function WolfgangHUD:SafeTableConcat(tbl, str)
